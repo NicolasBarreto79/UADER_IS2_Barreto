d4b1952f8877ec5fb2e616396894950fd242e0d9
Nicolas Barreto <barretonicolas810@gmail.com>
2025-03-24 20:08:00 -0300

    factorial modificado

--- src/factorial.py
+++ src/factorial.py
@@ -6,23 +6,24 @@
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
 import sys
+
 def factorial(num): 
     if num < 0: 
         print("Factorial de un número negativo no existe")
         return 0
     elif num == 0: 
         return 1
-        
     else: 
         fact = 1
-        while(num > 1): 
+        while num > 1: 
             fact *= num 
             num -= 1
         return fact 
 
-if len(sys.argv) == 0:
-   print("Debe informar un número!")
-   sys.exit()
-num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
+if len(sys.argv) < 2:
+    num = int(input("Debe informar un número: "))
+else:
+    num = int(sys.argv[1])
+
+print("Factorial", num, "! es", factorial(num))
 
